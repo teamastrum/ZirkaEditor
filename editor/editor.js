ec01c6aa9ede0173cb99866c759322322b4b664f
xslendix <gamingxslendix@gmail.com>
2019-08-01 17:03:07 +0300

    Fixed stuff

--- editor/editor.js
+++ editor/editor.js
@@ -1,18 +1,47 @@
 const { dialog } = require('electron');
 const files = require('./files');
-currentFiles = new Set();
-currentFile = null;
+
+// The main editor class.
+exports.editor = class {
+    constructor () {
+        this.openedFiles = [];
+        this.currentFile = new files.file(null);
+    }
+}
 
 exports.showError = (msg) => {
+    // Show Error dialog
     dialog.showErrorBox("Bruh moment", msg);
     return true;
 }
 
 exports.getCurrentFile = () => {
+    // TODO: Add file implementation in editor (After editor GUI is done and file editor is completed.)
     var file = new files.file('yes.txt', 'yes');
     return file;
 }
 
-exports.updateCurrentFileData = (data, value) => {
+exports.getAllOpenFiles = () => {
+    // Gets all currently opened files.
+    // TODO: Add implementation
+    return [];
+}
 
+exports.getAllFiles = () => {
+    // Gets all project files.
+    // TODO: Add implementation
+    return [];
+}
+
+exports.updateCurrentFileData = (data, value) => {
+    // Go to files.js for refrence.
+    // This will update the GUI of the editor in the current file.
+    exports.updateFileData(exports.getCurrentFile(), data, value);
+    return true;
+}
+
+exports.updateFileData = (file, data, value) => {
+    // Update file data (GUI).
+    // TODO: Add implementation.
+    return true;
 }
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Electron
 const electron = require('electron');
 
 // Enable live reload for all the files inside your project directory
@@ -5,15 +6,18 @@ require('electron-reload')(__dirname, {
     electron: require(`${__dirname}/node_modules/electron`)
 });
 
+// More electron stuff
 const { app, BrowserWindow } = electron;
 const url = require('url');
 const path = require('path');
 const Menu = electron.Menu;
 
+// Required
 const editor = require('./editor/editor');
 const files = require('./editor/files');
 
 function createWindow() {
+    // Create a new BrowserWindow
     let win = new BrowserWindow({
         width: 1920,
         height: 1080,
@@ -23,34 +27,44 @@ function createWindow() {
         }
     });
 
+    // Load index.html (Start Page)
     win.loadURL(url.format({
         pathname: path.join(__dirname, '/files/html/index.html'),
         protocol: 'file:',
         slashes: true
     }));
 
-    
     // Open Dev Tools
     win.webContents.openDevTools();
 
+    // If window is ready to show
     win.on('ready-to-show', () => {
+        // then show it
         win.show();
     });
 
+    // If window is closed
     win.on('closed', () => {
+        // Set it to null
         win = null;
     });
 
 }
 
+// If app is ready
 app.on('ready', () => {
+    // Create the window
     createWindow();
 
+    // Set menu to null
     Menu.setApplicationMenu(null);
+    
+    // To be removed when ctual editor added.
     setMenuBar();
 });
 
 function setMenuBar() {
+    // The actual menu for the editor.
     const template = [
         {
             label: 'File',
@@ -96,23 +110,24 @@ function setMenuBar() {
         }
     ];
 
+    // Set the menu.
     const menu = Menu.buildFromTemplate(template);
     Menu.setApplicationMenu(menu);
 
 }
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
+    // When all windows are closed
+    if (process.platform !== 'darwin') { // and the platform is not darwin
+        // then quit the app
         app.quit();
     }
 });
 
+// When activated
 app.on('activate', () => {
-    if (win == null) {
+    if (win == null) { // and win is not null
+        // then create the window
         createWindow();
     }
 });
-
-// app.on('open-file', (filePath) => {
-//    editor.openFile(filePath);
-// });
